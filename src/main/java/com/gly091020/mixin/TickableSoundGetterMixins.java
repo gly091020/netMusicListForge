240f60307bff8ef226563d1f0f33c90a8fabdfcb
gly091020 <gulangyu1020@qq.com>
2025-09-26 21:05:55 +0800

    去除多余mixin

--- src/main/java/com/gly091020/mixin/TickableSoundGetterMixins.java
+++ src/main/java/com/gly091020/mixin/TickableSoundGetterMixins.java
@@ -1,14 +1,11 @@
 package com.gly091020.mixin;
 
-import com.mojang.blaze3d.audio.Channel;
 import net.minecraft.client.resources.sounds.TickableSoundInstance;
-import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.client.sounds.SoundManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.List;
 
@@ -19,20 +16,11 @@ public interface TickableSoundGetterMixins {
         @Accessor("tickingSounds")
         @Final
         List<TickableSoundInstance> getTickableSoundInstances();
-
-        @Accessor("channelAccess")
-        @Final
-        ChannelAccess getChannelAccess();
     }
     @Mixin(SoundManager.class)
     interface SoundManagerMixin{
         @Accessor
         @Final
         SoundEngine getSoundEngine();
-    }
-    @Mixin(Channel.class)
-    interface ChannelMixin{
-        @Invoker("getState")
-        int invokeGetState();
     }
 }
